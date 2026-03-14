57830f9b08da70eb971ad250c4afe9fcc3498be6
Siyag Parulekar <parulekar.siyagavinash@students.iiserpune.ac.in>
2026-03-14 20:34:41 +0530

    Initial metadata pipeline

---
+++ src/query_system.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+conn = sqlite3.connect("../database/datasets.db")
+
+author = input("Enter author name: ")
+
+query = """
+SELECT title, authors, year, repository
+FROM datasets
+WHERE authors LIKE ?
+"""
+
+cursor = conn.cursor()
+
+cursor.execute(query, ("%" + author + "%",))
+
+results = cursor.fetchall()
+
+for r in results:
+    print(r)
+
+conn.close()
